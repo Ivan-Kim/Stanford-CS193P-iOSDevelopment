689f95c3b2505675bac262657a52375ecc216aaa
Ivan-Kim <57326300+Ivan-Kim@users.noreply.github.com>
2021-07-23 10:44:13 +0800

    add lecture6 demo

--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -18,7 +18,7 @@ class EmojiMemoryGame: ObservableObject {
                     "🏎", "🛻", "🛴", "🚝",]
     
     private static func createMemoryGame() -> MemoryGame<String> {
-        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+        MemoryGame<String>(numberOfPairsOfCards: 6) { pairIndex in
             emojis[pairIndex]
         }
     }
